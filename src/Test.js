211fae9139d02efaa0b9428cd8f10c822418bfe5
eclerchik13 <ler.poter2014@yandex.ru>
2020-12-21 17:52:36 +0300

    Add POST and GET requests, components for passing the test,time

--- src/Test.js
+++ src/Test.js
@@ -1,5 +1,119 @@
 import React, {useState, useEffect} from 'react'
 import {ButtonInput, InputField} from "./components/input";
+import {navigate} from "hookrouter";
+
+function InputQuestion(props){ //Enter answers
+    return(
+        <div className={"Question"}>
+            <div className={"InfoAboutSubject"}>{props.body}</div>
+            <InputField name={"Answer: "} class={"InputQuestion"} onChange={props.onChange}/>
+        </div>
+    )
+}
+
+///////////// - Test with questions and fields for answers - ////////
+
+function Test(props){ //props.data
+    let array = [];
+    for (let i=0; i < props.data.count;i++){
+        array.push({
+            answer:""
+        })
+    }
+    const[answers,setAnswers] = useState(array)
+
+    function SendAnswers(){
+        fetch('/api/test/'+props.data.id,{
+            method:'POST',
+            headers: {
+                'Content-Type':'application/json',
+            },
+            body: JSON.stringify({
+                userAnswers: answers
+            }),
+        })
+            .then(response => {return response.json();})
+            .then(data => {
+                if(data.message === "Answers saved"){
+                    navigate('/')//перенаправить на страницу пользователя
+                }
+                else if (data.message === "Error: Saving result"){
+                    //navigate('/error')//может navigate error with message
+                    console.error(data.message)
+                }
+            })
+    }
+
+    const submit = (e) => {
+        e.preventDefault();
+        SendAnswers(answers)
+    }
+
+    const ChangeAnswer = index => e =>{
+        let newState = answers;
+        newState[index].answer = e.target.value;
+        setAnswers(newState)
+    }
+
+    return(
+        <div className={"Test"}>
+            <div className={"head"}>
+                {props.data.title}
+            <p><Time ctime={props.data.time} onTimeOut={SendAnswers}/></p>
+            </div>
+            {
+                props.data.questions.map((item,index)=>{
+                    return(
+                        <p>
+                            <InputQuestion body={item.body} onChange={ChangeAnswer(index)}/>
+                        </p>
+                    )
+                })
+                 }
+            <ButtonInput function={"Send"} onClick={submit}/>
+            </div>
+    )
+}
+
+////////// - Main page with start test - /////////
+
+function Start(props){
+    const[test,setTest] =useState([])
+    function getTest(){
+        fetch('/api/test/'+props.id,{
+            method:'GET',
+            headers:{
+                'Content-Type':'application/json',
+            },
+        })
+            .then((response =>{return response.json()}))
+            .then(data => {
+                {
+                    setTest(data)
+                }
+            })
+    }
+    useEffect(() => {
+        getTest()
+    },[]);
+
+    const [state, setState] = useState({
+        addTest: false
+    })
+
+    const handleClick = () =>{
+        setState({addTest: true})
+    }
+    return(
+        <div className="Start">
+            <div className="head">Ready to start?</div>
+            <button className={'M'} onClick={handleClick}>Start</button>
+            {state.addTest && <Test data={test}/>}
+        </div>
+    )
+}
+
+///////////// - function Time - ////////////////
 
 const padTime = time =>{
     return String(time).length === 1 ? `0${time}`: `${time}`;
@@ -14,7 +128,8 @@ const format = time =>{
 }
 
 function Time(props){
-    const [counter, setCounter] = useState(props.time);
+    let data = props.ctime
+    const [counter, setCounter] = useState(data);
     useEffect(() => {
         let timer;
         if (counter > 0) {
@@ -29,79 +144,7 @@ function Time(props){
 
     return(
         <div className={"Timer"}>
-            {counter === 0 ? "Time over" : <div>Time:{format(counter)}</div> }
-        </div>
-    )
-}
-
-function InputQuestion(props){
-    return(
-        <div className={"Question"}>
-            <div className={"InfoAboutSubject"}>{props.body}</div>
-            <InputField name={"Answer: "} class={"InputQuestion"} onChange={props.onChange}/>
-        </div>
-    )
-}
-
-function Test(){
-    const [state, setState] = useState({
-        questions:[
-            {
-                question: "WTF"
-            },
-            {
-                question: "LOL"
-            }
-        ],
-        answers:[
-            {
-                answer: ""
-            },
-            {
-                answer: ""
-            }
-        ]
-    })
-
-    const submit = (e) => {
-        e.preventDefault();
-        alert(JSON.stringify(state));
-    }
-
-    const ChangeAnswer = index => e=>{
-        let oldState = state;
-        oldState.answers[index].answer = e.target.value;
-        setState(oldState)
-    }
-
-    return(
-        <div className={"Test"}>
-            <div className={"head"}>
-                Test
-            <p><Time time={"660"}/></p>
-            </div>
-        <p><InputQuestion body={state.questions[0].question} onChange={ChangeAnswer(0)}/></p>
-
-            <p><InputQuestion body={state.questions[1].question} onChange={ChangeAnswer(1)}/></p>
-
-            <ButtonInput function={"Send"} onClick={submit}/>
-            </div>
-    )
-}
-
-function Start(){
-    const [state, setState] = useState({
-        addTest: false
-    })
-
-    const handleClick = () =>{
-        setState({addTest: true})
-    }
-    return(
-        <div className="Start">
-            <div className="head">Ready to start?</div>
-            <button className={'M'} onClick={handleClick}>Start</button>
-            {state.addTest && <Test />}
+            {counter === 0 ? props.onTimeOut() : <div>Time:{format(counter)}</div> }
         </div>
     )
 }
