70dec189a2d84d79e0f4e08a917e1b8985251418
eclerchik13 <ler.poter2014@yandex.ru>
2020-12-21 17:52:01 +0300

    output of all tests across GET request

--- src/StartTest.js
+++ src/StartTest.js
@@ -1,16 +1,64 @@
-import React, {useState} from 'react'
-import {navigate} from "hookrouter";
-import {LinkQuestion} from "./components/input";
+import React, {useState,useEffect} from 'react'
+import {A, navigate} from "hookrouter";
 
-function Questions(){
+function LinkQuestion(props){
+    let id = props.data.id;
+    let title = props.data.title;
+    let time = props.data.time;
+    let count = props.data.count;
+    return(
+        <p><div className={"LinkQuestion"}>
+            <A href={"/test/"+id}>
+                <div className={"InfoTest"}>
+                    <p>Name: {title}</p>
+                    <p>Number of questions: {count}</p>
+                    <p>Time: {time+" seconds"}</p>
+                </div>
+            </A>
+        </div>
+        </p>
+    )
+}
+
+function Questions(props){
+    let newTemple = props.data.map((item,index) => {
+        return(
+            <div key={index}>
+                <LinkQuestion data={item}/>
+            </div>
+        )
+    })
     return(
         <div className={"AllQuestions"}>
             <div className={"head"}>Все тесты</div>
             <div className={"InfoAboutSubject"}>Выбирете тест, который хотите пройти.</div>
-            <LinkQuestion id={"1"} name={"Физика"} time={"11"} number={"5"} />
-            <LinkQuestion id={"2"} name={"Физика"} time={"10"} number={"10"} />
+            {newTemple}
         </div>
     )
 }
 
-export {Questions}
+function GetQuestions(){
+    const [tests,setTests] = useState([]);
+    function getTests(){
+        fetch('/api/tests',{
+            method:'GET',
+            headers:{
+                'Content-Type':'application/json',
+            },
+        })
+            .then((response =>{return response.json()}))
+            .then(data => {
+                {
+                    setTests(data)
+                }
+            })
+    }
+    useEffect(() => {
+        getTests()
+    },[]);
+    return (
+        <Questions data={tests}/>
+    );
+}
+
+export {Questions, GetQuestions}
